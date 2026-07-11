6bb034687c63210a1202e6a9010eea07b8225163
edbatt1002 <edbatt1002@users.noreply.github.com>
2026-07-11 10:19:32 -0300

    feat: register professional layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['./layers/shared'],
+  extends: ['./layers/shared', './layers/professionals'],
 
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils/module'],
 
