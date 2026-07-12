cc27ae65e87efb1d294260e4fa00c5f3e3f80e4a
edbatt1002 <edbatt1002@users.noreply.github.com>
2026-07-12 10:25:10 -0300

    chore(test): implement coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,17 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'node',
-    globals: true
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json-summary'],
+      include: ['layers/**/app/**/*.{ts,vue}'],
+      exclude: [
+        '**/__tests__/**',
+        '**/types.ts',
+        '**/*.config.*',
+        'layers/**/mock/**'
+      ]
+    }
   }
 })
