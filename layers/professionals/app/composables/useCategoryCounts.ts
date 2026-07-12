9ee1386f17d98dbdb0b95d427365b900bf02648e
edbatt1002 <edbatt1002@users.noreply.github.com>
2026-07-12 00:05:23 -0300

    feat: create category count composable

---
+++ layers/professionals/app/composables/useCategoryCounts.ts
@@ -0,0 +1,12 @@
+import { listProfessionals } from '../services/professionals'
+
+export function useCategoryCounts(slugs: string[]) {
+  return Object.fromEntries(slugs.map((slug) => {
+    const { data } = useServerQuery(
+      ['category-count', slug],
+      async () => (await listProfessionals({ profession: slug, limit: 1 })).meta.total,
+      { staleTime: 1000 * 60 * 60 }
+    )
+    return [slug, data]
+  }))
+}
