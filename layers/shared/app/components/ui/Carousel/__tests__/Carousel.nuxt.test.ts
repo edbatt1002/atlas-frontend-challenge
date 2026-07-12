669147346cb1047deacb57692c640bfd6fbfa6ab
edbatt1002 <edbatt1002@users.noreply.github.com>
2026-07-12 11:06:04 -0300

    test: add carousel tests structure

--- layers/shared/app/components/ui/Carousel/__tests__/Carousel.nuxt.test.ts
+++ layers/shared/app/components/ui/Carousel/__tests__/Carousel.nuxt.test.ts
@@ -1,4 +1,4 @@
-import { h } from 'vue'
+import { h, nextTick } from 'vue'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import Carousel from '../Carousel.vue'
 import CarouselSlide from '../../CarouselSlide/CarouselSlide.vue'
@@ -56,5 +56,30 @@ describe('Carousel', () => {
     await wrapper.find('[aria-label="Próximo slide"]').trigger('click')
 
     expect(wrapper.emitted('update:modelValue')).toEqual([[1]])
+  })
+
+  it('emits update:modelValue when the prev button is activated', async () => {
+    const wrapper = await mountSuspended(Carousel, {
+      props: { slideCount: 3, navigation: true, modelValue: 2 },
+      slots: { default: () => h(CarouselSlide, null, () => 'Slide 1') }
+    })
+
+    await wrapper.find('[aria-label="Slide anterior"]').trigger('click')
+
+    expect(wrapper.emitted('update:modelValue')).toEqual([[1]])
+  })
+
+  it('updates the model from a swiper slide-change event', async () => {
+    const wrapper = await mountSuspended(Carousel, {
+      props: { slideCount: 3, modelValue: 0 },
+      slots: { default: () => h(CarouselSlide, null, () => 'Slide 1') }
+    })
+
+    wrapper.find('swiper-container').element.dispatchEvent(
+      new CustomEvent('swiperslidechange', { detail: [{ activeIndex: 2 }] })
+    )
+    await nextTick()
+
+    expect(wrapper.emitted('update:modelValue')).toEqual([[2]])
   })
 })
